c42c100d4c63792e44bdb137313897e679c1ca87
Matthias Buchholz <matthias@afkm.io>
2018-02-09 11:56:52 +0100

    no output on console

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+/// <reference name="node" />
+
 import { XHRWorker } from './classes/worker.class'
-const packageInfo:any = require('../package.json')
+//const packageInfo:any = require('../package.json')
 
 declare const self:Worker
-
+/*
 console.log(
   '%c%s %cv%s', 
   'font-size: 10px; color: #aaa; font-weight: bold;', 
   packageInfo.name, 
   'font-size: 10px; color: #AAA; font-weight: lighter;', 
   packageInfo.version 
-)
+)*/
 const worker = new XHRWorker(self)
